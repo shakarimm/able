5f4141747a8f920deaddc3792e11c61ebf7051fa
Shakarim <mukhamediyev71636@gmail.com>
2022-08-31 22:30:07 +0600

    half of home page

---
+++ src/js/custom_modules/Countup.js
@@ -0,0 +1,74 @@
+/*
+--------------------------------------------
+--------------------------------------------
+		  		COUNTUP
+--------------------------------------------
+--------------------------------------------
+*/
+function Countup(block) {
+	let classes = {
+		base: 'tmpl-hh__countup',
+		withoutSeparate: 'tmpl-hh__countup--without-separate',
+		initialized: 'tmpl-hh__countup--initialized'
+	};
+
+	let duration = 2000,
+		frameCount = 1000 / 60;
+
+	let easeOutCubic = function (val) {
+		return 1 - Math.pow(1 - val, 3);
+	};
+	let animate = function (el) {
+		if(el.classList.contains(classes.initialized)){
+			return false;
+		}
+		el.classList.add(classes.initialized);
+
+		let frame = 0,
+			totalFrames = Math.round(duration / frameCount),
+			dataVal = el.getAttribute('data-value').replace(/,/, "."),
+			decimalCount = parseInt(el.dataset.decimalCount || 0),
+			decimalSymbol = el.dataset.decimalSymbol || "",
+			thousandSymbol = el.dataset.thousandSymbol || " ",
+			isFloat = dataVal.indexOf(',') !== -1 || dataVal.indexOf('.') !== -1,
+			countTo = parseFloat(dataVal);
+
+		let counter = setInterval(function(){
+			frame++;
+
+			const progress = easeOutCubic(frame / totalFrames);
+			let currentCount = countTo * progress;
+
+			if(isFloat){
+				currentCount = currentCount.toFixed(1).toString();
+			}else{
+				currentCount = Math.round(currentCount);
+			}
+
+			if (parseFloat(el.innerHTML) !== currentCount) {
+				if(currentCount > 999 && !el.classList.contains(classes.withoutSeparate)){
+					currentCount = currentCount.replace(/\B(?=(\d{3})+(?!\d))/g, thousandSymbol);
+				}
+				el.innerHTML = currentCount;
+			}
+
+			if (frame === totalFrames) {
+				clearInterval(counter);
+			}
+		}, frameCount);
+	};
+	let init = function () {
+		if (typeof block === "string")
+			block = document.querySelector(block);
+		let blockElement = block;
+		let items = blockElement.getElementsByClassName(classes.base);
+
+		for (let i = 0; i < items.length; i++){
+			animate(items[i]);
+		}
+	};
+
+	init();
+}
+
+export default Countup;
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+import VSwiper from "./modules/VSwiper";
+import Countup from './custom_modules/Countup';
+import Video from "./modules/Video";
+import Content from "./modules/Content";
+import Nav from "./modules/Nav";
 /*
 	--------------------------------------------
 	--------------------------------------------
@@ -5,8 +10,34 @@
 	--------------------------------------------
 	--------------------------------------------
  */
+	function initCompanySlider() {
+		swiper.init(".tmpl-hh__company-slider", {
+			loop: true,
+			slidesPerView: 6,
+			autoHeight: true,
+			bulletActiveClass: '.tmpl-hh__company-slider-pagination-active',
+			pagination: {
+				el: '.tmpl-hh__company-slider__pagination',
+				clickable: true,
+			},
+		});
+	}
 
-
+	const scrollElements = document.querySelectorAll(".tmpl-hh__s-stats");
+	const elementInView = (el, dividend = 1) => {  
+		const elementTop = el.getBoundingClientRect().top;  
+		return (    
+			elementTop <=    
+			(window.innerHeight || document.documentElement.clientHeight) / dividend  );};
+			const handleScrollAnimation = () => {  
+				scrollElements.forEach((el) => {    
+					if (elementInView(el, 1.25)) {      
+						new Countup(".tmpl-hh__s-stats");
+					}  
+				})
+			}
+			window.addEventListener("scroll", () => {   handleScrollAnimation();});
+			
 /*
 	--------------------------------------------
 	--------------------------------------------
@@ -14,3 +45,10 @@
 	--------------------------------------------
 	--------------------------------------------
  */
+
+	const swiper = new VSwiper();
+	new Video();
+	new Content();
+	new Nav();
+
+	initCompanySlider()
