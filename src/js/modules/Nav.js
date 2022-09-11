4b7492f2c60d67fc6989d31f31c44af8c446080b
Shakarim <mukhamediyev71636@gmail.com>
2022-09-11 09:44:18 +0600

    nav childs

--- src/js/modules/Nav.js
+++ src/js/modules/Nav.js
@@ -26,6 +26,39 @@ function Nav() {
 		btn.classList.remove(classes.btn.closed);
 		btn.classList.add(classes.btn.active);
 	};
+
+	let headerNav = document.querySelector('.header__nav')
+	let tabs = document.querySelectorAll('.content-tab')
+	// let navs = document.querySelectorAll('.nav__item--parent')
+	headerNav.addEventListener('click', function () {
+		opened = true;
+		nav.classList.add(classes.nav.opened);
+		btn.classList.remove(classes.btn.closed);
+		btn.classList.add(classes.btn.active);
+	})
+	for (let tab of tabs){
+		tab.addEventListener('click', function (event) {
+			event.stopPropagation()
+			opened = false;
+			nav.classList.remove(classes.nav.opened);
+			btn.classList.remove(classes.btn.active);
+			btn.classList.add(classes.btn.closed);
+		})
+	}
+	// let currentNav = null
+	// for (let nav of navs){
+	// 	nav.addEventListener('click', () => {
+	// 		if (currentNav === nav){
+	// 			nav.firstElementChild.classList.remove('nav__item-menu--active')
+	// 			currentNav = null
+	// 		}else{
+	// 			navs.forEach(nav => nav.firstElementChild.classList.remove('nav__item-menu--active'))
+	// 			nav.firstElementChild.classList.add('nav__item-menu--active')
+	// 			currentNav = nav
+	// 		}
+	// 	})
+	// }
+
 	let toggle = function () {
 		if(opened){
 			close();
